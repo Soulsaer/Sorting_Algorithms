642b0160c0c842d723365981e94f1782e6d1250f
Soulsaer <97550940+Soulsaer@users.noreply.github.com>
2023-08-03 01:58:32 +0530

    Add files via upload

---
+++ Bubble_sort.cpp
@@ -0,0 +1,40 @@
+// Bubble Sort Algorithm
+// input : size(n), arr[];
+// ouput : sorted array
+
+#include<iostream>
+using namespace std;
+
+void bubble_sort(int arr[],int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+int main()
+{
+    int n =0;
+    cin>> n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    bubble_sort(arr,n);
+    cout<<"Sorted array : ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+}
